b73f91d37e35d4d967d257d2bdc95ad6bc23ccb9
Andrea Botello <abotellod@gmail.com>
2026-08-18 08:23:47 -0500

    Hall. Repo [2.0]

--- src/config/global.js
+++ src/config/global.js
@@ -297,7 +297,7 @@ export default {
         {
           nombre: 'Claudia Johanna Gómez Pérez',
           cargo:
-            'Profesional G06. Responsable Ecosistema de Recursos Educativos Digitales (RED)',
+            'Profesional G06. Responsable Ecosistema de Recursos Educativos Digitales',
           centro: 'Centro Agroturístico - Regional Santander',
         },
         {
